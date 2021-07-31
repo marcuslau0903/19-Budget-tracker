5c2cfba388c81197fcd176b923c13b8fa71a96eb
MarcusLau <marcuslau0903@gmail.com>
2021-07-31 23:15:13 +0100

    deploying

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const app = express();
+app.use(logger("dev"));
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+// routes
+app.use(require("./routes/api.js"));
+const init = async () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+init();
